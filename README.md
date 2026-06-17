24675c600cbf42a6b28724f6ccfac26f61c277e2
Jerome Haltom <jhaltom@alethic.solutions>
2026-06-17 09:24:50 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GeoDesk for .NET
+
+This is a recreation of the GeoDesk Java project in C#. It is a work in progress, and is not yet complete. The goal of this project is to create a .NET library for geospatial data processing, similar to the GeoDesk Java library.
---
+++ src/GeoDesk.Tests/CalciteConnectionPropertiesTests.cs
@@ -0,0 +1,83 @@
+﻿using java.util;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using org.apache.calcite.avatica.util;
+using org.apache.calcite.config;
+using org.apache.calcite.sql.validate;
+
+namespace Apache.Calcite.Data.Tests
+{
+
+    [TestClass]
+    public class CalciteConnectionPropertiesTests
+    {
+
+        [TestMethod]
+        public void TestDefaults()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            Assert.AreEqual(false, c.ApproximateDecimal);
+            Assert.AreEqual(false, c.ApproximateDistinctCount);
+            Assert.AreEqual(false, c.ApproximateTopN);
+            Assert.AreEqual(false, c.AutoTemp);
+            Assert.AreEqual(false, c.CaseSensitive);
+            Assert.AreEqual(SqlConformanceEnum.DEFAULT, c.Conformance);
+            Assert.AreEqual(NullCollation.HIGH, c.DefaultNullCollation);
+            Assert.AreEqual("standard", c.Fun);
+            Assert.AreEqual(Lex.ORACLE, c.Lex);
+            Assert.AreEqual("", c.Locale);
+            Assert.AreEqual(null, c.QuotedCasing);
+            Assert.AreEqual(null, c.UnquotedCasing);
+        }
+
+        [TestMethod]
+        public void CanSetApproximateDecimal()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            c.ApproximateDecimal = true;
+            Assert.AreEqual(true, c.ApproximateDecimal);
+        }
+
+        [TestMethod]
+        public void CanSetConformance()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            c.Conformance = SqlConformanceEnum.BIG_QUERY;
+            Assert.AreEqual(SqlConformanceEnum.BIG_QUERY, c.Conformance);
+        }
+
+        [TestMethod]
+        public void CanSetQuotedCasing()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            c.QuotedCasing = Casing.TO_LOWER;
+            Assert.AreEqual(Casing.TO_LOWER, c.QuotedCasing);
+        }
+
+        [TestMethod]
+        public void CanSetQuotedCasingToNull()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            c.QuotedCasing = null;
+            Assert.AreEqual(null, c.QuotedCasing);
+        }
+
+        [TestMethod]
+        public void CanSetSchemaProperty()
+        {
+            var p = new Properties();
+            var c = new CalciteConnectionProperties(p);
+            c.SchemaProperties["host"] = "localhost";
+            Assert.AreEqual("localhost", c.SchemaProperties["host"]);
+            Assert.AreEqual("localhost", p.getProperty("schema.host"));
+        }
+
+    }
+
+}
